4eb4bb985cc878f7ad2e072c8b7baa2f8b840f94
GabrielF157 <gabrielsouza2006.2005@gmail.com>
2025-10-22 15:23:15 -0400

    Adicionando questao 2

--- parte_2_Pratica/questao02/questão2.c
+++ parte_2_Pratica/questao02/questão2.c
@@ -1,9 +1,48 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-
-
-
-return 0;
+// Função que cria dinamicamente um vetor de n elementos
+int* criarVetor(int n) {
+    int *vetor = (int*) malloc(n * sizeof(int)); // aloca espaço para n inteiros
+    return vetor;
 }
 
+// Função que imprime os elementos de um vetor
+void imprimirVetor(int *vetor, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+}
+
+// Função que libera a memória alocada
+void liberarVetor(int *vetor) {
+    free(vetor);
+}
+
+int main() {
+    int n, i;
+    int *vetor;
+
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &n);
+
+    // Criação do vetor
+    vetor = criarVetor(n);
+
+    // Leitura dos elementos
+    printf("Digite os elementos do vetor:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    // Impressão dos elementos
+    printf("Elementos do vetor: ");
+    imprimirVetor(vetor, n);
+
+    // Liberação da memória
+    liberarVetor(vetor);
+
+    return 0;
+}
